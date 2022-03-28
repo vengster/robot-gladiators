187324e82f0bc2d65cc51fdf4201be0f50d51650
vengsterpc <veng.lee@gmail.com>
2022-03-28 10:13:36 -0700

    added some functions to game.js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,17 @@
-window.alert("this is an alert! JavaScript is running!");
+// this creats a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt ("what is your robot's name?");
+
+// note lack of quotation marks around playerName
+console.log(playerName);
+console.log("this logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10+10);
+// whatis is this?
+console.log("Our robot's name is " + playerName);
+
+// fight();
